b85a4a617f41d48da40e6c95321642911db0f9fd
Julien LAURENT <julienlaurent77@outlook.com>
2024-02-16 12:18:30 +0100

    make thank controller and set return datas from db

---
+++ src/Controller/ThanksController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Thanks;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ThanksController extends AbstractController
+{
+    public function __construct(private EntityManagerInterface $entityManager) {}
+
+    #[Route('/thanks', name: 'app_thanks')]
+    public function index(): Response
+    {
+        $thanks = $this->entityManager->getRepository(Thanks::class)->findAll();
+        
+        return $this->render('thanks/index.html.twig', [
+            'thanks' => $thanks,
+        ]);
+    }
+}
